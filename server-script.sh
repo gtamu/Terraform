214eecaae4a07359d695cf7488c01c53f58efafb
Giri Gurung <giri.gurung@ibm.com>
2019-12-12 00:07:26 -0600

    modified script

--- server-script.sh
+++ server-script.sh
@@ -3,4 +3,4 @@ sudo yum update
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<html><h1>Created by Terraform Script</h1><a href="https://github.com/gtamu/code-challenge">Github</a></html>" | sudo tee /var/www/html/index.html
+echo "<html><h1>Created by Terraform Script</h1><a href="https://github.com/gtamu/terraform">Github</a></html>" | sudo tee /var/www/html/index.html
